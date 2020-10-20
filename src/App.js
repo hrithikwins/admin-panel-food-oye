978498ec4219780ec01e7ca18a77448cfb508454
HRITHIK <hrithikrtiwari@gmail.com>
2020-10-20 23:58:55 +0530

    completed content list Only pending is to add a fetch by username

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import {ContentsList} from './views/content-list';
-import {EditContent} from './views/edit-content';
-import {CreateContent} from './views/create-content';
-import {CreateUser} from './views/create-user';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar} from './components/navbar';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ContentsList } from "./views/content-list";
+import { EditContent } from "./views/edit-content";
+import { CreateContent } from "./views/create-content";
+import { CreateUser } from "./views/create-user";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
     <>
-    <Router>
-    <Navbar></Navbar>
-      <Route exact path="/" component={ContentsList}></Route>
-      <Route exact path="/edit/:id" component={EditContent}></Route>
-      {/* <Route exact path="/create-user" component={CreateContent}></Route> */}
-      <Route exact path="/create-user" component={CreateUser}></Route>
-    </Router>
+      <Router>
+        <Navbar></Navbar>
+        <Route exact path="/" component={ContentsList}></Route>
+        <Route exact path="/edit/:id" component={EditContent}></Route>
+        {/* <Route exact path="/create-user" component={CreateContent}></Route> */}
+        <Route exact path="/create-user" component={CreateUser}></Route>
+        <Route exact path="/create-user" component={CreateUser}></Route>
+        <Route exact path="/create-user" component={CreateUser}></Route>
+      </Router>
     </>
   );
 }
--- src/views/content-list.jsx
+++ src/views/content-list.jsx
@@ -1,17 +1,8 @@
-import { Button, Grid, TextField, Accordion, AccordionSummary, AccordionDetails, Typography } from "@material-ui/core";
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Grid, Button, Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from "@material-ui/core";
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import axios from 'axios';
 import React, { useState, useEffect } from "react";
 import { CreateUser } from "./create-user";
-
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
 
 
 export const ContentsList = () => {
@@ -130,8 +121,24 @@ export const ContentsList = () => {
                                         );
                                     })}
                                     <TableCell>
-
-                                        View | Manage
+                                        <Link to="user-info">
+                                            <Button
+                                                variant="contained"
+                                                color="primary"
+                                                startIcon={<EditOutlinedIcon />}
+                                            >
+                                                View
+                                            </Button>
+                                        </Link> |
+                                        <Link to="user-content">
+                                            <Button
+                                                variant="contained"
+                                                color="secondary"
+                                                startIcon={<EditOutlinedIcon />}
+                                            >
+                                                Manage
+                                            </Button>
+                                        </Link>
                                     </TableCell>
                                 </TableRow>
                             );
